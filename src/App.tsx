85828a156535fc6755fe53523b38a3f3d39ca3fb
be wang <be_wang@mail.chungyo.net>
2025-06-13 11:20:30 +0800

    應用置中樣式設計改進
    1. 添加主容器讓應用內容置中顯示
    2. 改進背景和佈局樣式
    3. 添加陰影和圓角效果
    4. 優化 TodoList 組件的樣式

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,23 @@ import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <UserProvider>
-      <HashRouter>
-        <Routes>
-          {/* 公開路由 */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
+      <div className="app-container max-w-4xl mx-auto p-4">
+        <HashRouter>
+          <Routes>
+            {/* 公開路由 */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
 
-          {/* 受保護路由 - 不需要登入 */}
-          <Route element={<ProtectedRoute requireAuth={false} />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
+            {/* 受保護路由 - 不需要登入 */}
+            <Route element={<ProtectedRoute requireAuth={false} />}>
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </Route>
 
-          {/* 重定向錯誤路徑 */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </HashRouter>
+            {/* 重定向錯誤路徑 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </HashRouter>
+      </div>
     </UserProvider>
   );
 }
